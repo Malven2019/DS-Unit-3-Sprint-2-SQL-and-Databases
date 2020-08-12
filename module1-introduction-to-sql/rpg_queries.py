7b2ff8de870bc1c0f63a190a0a8a6a26bac6671a
Malven Mtombeni <malmtombeni@gmail.com>
2020-08-12 03:26:00 -0400

    Updated insert_titanic.py and colab notebook

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -14,6 +14,9 @@ cursor = connection.cursor()
 # How many total characters?
 # In Table charactercreator_character
 
+# Commenting out the code on why they are taking a specific action
+# print(len(c1.execute('SELECT * FROM charactercreator_character;').fetchall()[0][2:]))
+
 count_characters = "SELECT COUNT(*) FROM charactercreator_character;"
 print("The total number of characters is",
       cursor.execute(count_characters).fetchall())
@@ -59,26 +62,6 @@ print("The number of non-weapon armory items is:", armory_not_weapons)
 
 # How many Items does each character have? (Return first 20 rows)
 
-# merged_query = '''
-# SELECT * FROM (charactercreator_character ,
-# charactercreator_mage)
-# WHERE
-# charactercreator_character.character_id = charactercreator_mage.character_ptr_id;
-# '''
-# print(cursor.execute(merged_query).fetchall())
-
-# merged1_query ='''
-# SELECT * FROM
-# charactercreator_character AS cc,
-# armory_item AS ai,
-# charactercreator_character_inventory AS cci
-# WHERE
-# cc.character_id = cci.character_id AND
-# ai.item_id = cci.item_id;
-#
-# '''
-# print(cursor.execute(merged1_query).fetchall())
-
 item_number = '''
 SELECT cc.character_id, COUNT(ai.item_id)
 FROM
@@ -94,25 +77,6 @@ LIMIT 20;
 print(cursor.execute(item_number).fetchall())
 
 # How many Weapons does each character have? (Return first 20 rows)
-
-# merged_query = '''
-# SELECT * FROM (charactercreator_character ,
-# charactercreator_mage)
-# WHERE
-# charactercreator_character.character_id = charactercreator_mage.character_ptr_id;
-# '''
-# print(cursor.execute(merged_query).fetchall())
-
-# merged1_query ='''
-# SELECT * FROM
-# charactercreator_character AS cc,
-# armory_weapon AS aw,
-# charactercreator_character_inventory AS cci
-# WHERE
-# cc.character_id = cci.character_id AND
-# aw.item_ptr_id = cci.item_id;
-# '''
-# print(cursor.execute(merged1_query).fetchall())
 
 weapon_number = '''
 SELECT cc.character_id, COUNT(aw.item_ptr_id)
